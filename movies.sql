398b8b76029a3ad0528e825d6f420c64d06e2e4e
Candace <2100978@talnet.nl>
2020-03-09 12:13:12 +0100

    Uitwerking van de exercise

---
+++ movies.sql
@@ -0,0 +1,23 @@
+
+DROP table films;
+Create Table films (
+	 id int unique auto_increment,
+    title varchar(20),
+    duur float,
+    datum date,
+    land varchar(20),
+    omschrijving varchar (80),
+    TrailerId varchar(60)
+    
+);
+
+SELECT * FROM films;
+   show tables;
+
+INSERT INTO films (title)
+	values('My little pony');
+   
+   
+
+
+
